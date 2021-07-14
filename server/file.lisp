0656b5f0e4734dd5c1012f87babf495a7e5fca88
John Hilts <johnhilts@live.com>
2021-07-14 16:55:53 -0700

    content viewer - add file-info sub-type for content-info

--- server/file.lisp
+++ server/file.lisp
@@ -1,23 +1,35 @@
 (in-package #:content-viewer)
 
-(define-info-class file folders images videos) ;; note: I can't inline this - it will break compilation if I don't do this on the top level
+(define-info-class file path timestamp content-type)
+(define-info-class content folders images videos) ;; note: I can't inline this - it will break compilation if I don't do this on the top level
 
-(defun get-files (directory)
-  (flet ((get-files-by-type (wildcards)
-           (remove-if #'null (mapcan #'(lambda (wildcard) (directory (format nil "~a/~a" directory wildcard))) wildcards))))
+(defun get-content-files (directory)
+  (flet ((get-content-files-by-type (wildcards content-type)
+           (mapcar #'(lambda (path)
+                       (let ((timestamp (get-universal-time))
+                             (file (make-instance 'file-info)))
+                         (populate-info-object file path timestamp content-type)))
+                   (remove-if #'null
+                              (mapcan #'(lambda (wildcard) (directory (format nil "~a/~a" directory wildcard))) wildcards)))))
     (let ((folder-mask '("*"))
           (image-mask '("*.png" "*.jpg"))
           (video-mask '("*.mov" "*.mp4")))
       (multiple-value-bind
             (folders images videos)
-            (values
-             (cons directory (get-files-by-type folder-mask))
-             (get-files-by-type image-mask)
-             (get-files-by-type video-mask))
-            (let ((file (make-instance 'file-info)))
-              (populate-info-object file folders images videos))))))
+          (values
+           (get-content-files-by-type folder-mask 'folder)
+           (get-content-files-by-type image-mask 'image)
+           (get-content-files-by-type video-mask 'video))
+        (let ((content (make-instance 'content-info)))
+          (populate-info-object content folders images videos))))))
 
-(defmethod index-folders ((file-info file-info))
-  (let* ((file-folders (file-folders file-info))
-         (folder-count (length file-folders)))
-    (make-array folder-count :element-type 'pathname :initial-contents file-folders)))
+(defmethod index-folders ((content-info content-info))
+  (let* ((content-folders (content-folders content-info))
+         (folder-count (length content-folders)))
+    (make-array folder-count :element-type 'pathname :initial-contents content-folders)))
+
+(defparameter *content-root* "./media"
+  "root of content media")
+
+(defparameter *folders* (index-folders (get-content-files "./media"))
+  "array of indexed folders")
