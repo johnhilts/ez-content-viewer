93fce99b01703b759247b97fc2ee89a729c67491
John Hilts <johnhilts@live.com>
2021-07-14 22:30:47 -0700

    content viewer - big refactor + get timestamp from exif

--- server/file.lisp
+++ server/file.lisp
@@ -3,25 +3,31 @@
 (define-info-class file path timestamp content-type)
 (define-info-class content folders images videos) ;; note: I can't inline this - it will break compilation if I don't do this on the top level
 
+(defmethod get-content-timestamp ((file-info file-info))
+  (case (file-content-type file-info)
+    (:image
+     (let ((exif (make-exif (file-path file-info))))
+       (exif-value :DateTimeOriginal exif)))
+    (:video
+     (format t "this is a video"))
+    (otherwise
+     (format t "this is something else"))))
+
 (defun get-content-files (directory)
-  (flet ((get-content-files-by-type (wildcards content-type)
-           (mapcar #'(lambda (path)
-                       (let ((timestamp (get-universal-time))
-                             (file (make-instance 'file-info)))
-                         (populate-info-object file path timestamp content-type)))
-                   (remove-if #'null
-                              (mapcan #'(lambda (wildcard) (directory (format nil "~a/~a" directory wildcard))) wildcards)))))
-    (let ((folder-mask '("*"))
-          (image-mask '("*.png" "*.jpg"))
-          (video-mask '("*.mov" "*.mp4")))
-      (multiple-value-bind
-            (folders images videos)
-          (values
-           (get-content-files-by-type folder-mask 'folder)
-           (get-content-files-by-type image-mask 'image)
-           (get-content-files-by-type video-mask 'video))
-        (let ((content (make-instance 'content-info)))
-          (populate-info-object content folders images videos))))))
+  (defun get-content-files-by-type (wildcards content-type)
+    (flet ((get-pathnames-by-type (wildcards)
+             (remove-if #'null
+                        (mapcan #'(lambda (wildcard) (directory (format nil "~a/~a" directory wildcard))) wildcards)))
+           (get-file-info (path)
+             (let ((timestamp (get-universal-time))
+                   (file (make-instance 'file-info)))
+               (populate-info-object file path timestamp content-type))))
+      (mapcar #'get-file-info (get-pathnames-by-type wildcards))))
+  (let ((folders (get-content-files-by-type  '("*") :folder))
+        (images (get-content-files-by-type '("*.png" "*.jpg" "*.PNG" "*.JPG") :image))
+        (videos (get-content-files-by-type '("*.mov" "*.mp4") :video))
+        (content (make-instance 'content-info)))
+    (populate-info-object content folders images videos)))
 
 (defmethod index-folders ((content-info content-info))
   (let* ((content-folders (content-folders content-info))
